dd5b00b902214445e95a8813f5637759f9a1e0d2
aravind <aravind284479@gmail.com>
2025-03-12 21:47:04 +0530

    feat: brand name bug

--- src/components/ui/brand-name.tsx
+++ src/components/ui/brand-name.tsx
@@ -1,19 +1,25 @@
-import Image from "next/image";
 import React from "react";
+import { CgEditUnmask } from "react-icons/cg";
 
 const BrandName = ({ size }: { size: number }) => {
   return (
-    <Image
+    <p
       style={{
+        fontSize: size,
         height: size,
       }}
-      objectFit="contain"
-      className="object-contain w-fit flex"
-      src={"/images/app.png"}
-      height={size}
-      width={size * 100}
-      alt="NeuroFill Logo"
-    />
+      className={`font-bold leading-tight bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent gap-1 flex items-center ${
+        size ? "" : "brandname"
+      }`}
+    >
+      <CgEditUnmask
+        style={{
+          fontSize: size - 5,
+        }}
+        className="text-primary"
+      />{" "}
+      NeuroFill
+    </p>
   );
 };
 
